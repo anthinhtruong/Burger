96a97d71444b07490b1faf1c981b7c86e01776ac
An Thanh Truong <antruong@Ans-MacBook-Pro.local>
2017-06-25 17:44:55 -0700

    Some Connected

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+insert into burger (burger_name, devoured) values ("Double Cheese Burger", false);
+insert into burger (burger_name, devoured) values ("Bacon and Cheese Burger", false);
+insert into burger (burger_name, devoured) values ("Fish and Cheese Burger", false);
---
+++ models/burger.js
@@ -0,0 +1,24 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function(){
+        orm.selectAll("burger", function(res) {
+            cb(res);
+        });
+    },
+
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burger", cols, vals, function(res) {
+            cb(res);
+    });
+  },
+
+    updateOn: function(objColVals, condition, cb) {
+    orm.updateOn("burger", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+
+}
+
+module.exports = burger;
